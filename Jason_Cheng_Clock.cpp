864e65480d76762c547a85ec875e03fefba8da5e
JasonCheng31 <164652284+JasonCheng31@users.noreply.github.com>
2024-03-23 11:33:18 -0400

    Jason_Cheng_Clock.cpp

--- Jason_Cheng_Clock.cpp
+++ Jason_Cheng_Clock.cpp
@@ -14,7 +14,7 @@ public:
             cout << endl;
         }
 
-        else if (Phours > 24 || Pminutes >= 60 || Pseconds >= 60 || Phours <= 0 || Pminutes < 0 || Pseconds < 0)
+        else if (Phours > 24 || Pminutes >= 60 || Pseconds >= 60 || Phours <= 0 || Pminutes < 0 || Pseconds < 0) //Check if the clock is out of bound and if it is, then we set 0 to Phours, Pminutes, Pseconds
         {
             cout << "Invalid input. Time reseted to 0 for this time: " << Phours << ":" << Pminutes << ":" << Pseconds << endl;
             Phours = 0;
@@ -50,7 +50,7 @@ Time operator+(Time &a, Time &b)
     int Addhours = a.getHours() + b.getHours();
     int Addminutes = a.getMinutes() + b.getMinutes();
     int Addseconds = a.getSeconds() + b.getSeconds();
-    if (Addseconds >= 60)
+    if (Addseconds >= 60)      //If the numbers being added is equal or past 60, I subtract 60 from that number. For example when a clock hits 60 seconds, it goes back to 0 second and adds 1 to minutes. If it's 61 second it would be 1 second in clock time. 
     {
         int value = Addseconds - 60;
         Addseconds = value;
@@ -76,7 +76,7 @@ Time operator-(Time &a, Time &b)
     int Minushours = a.getHours() - b.getHours();
     int Minusminutes = a.getMinutes() - b.getMinutes();
     int Minusseconds = a.getSeconds() - b.getSeconds();
-    if (Minusseconds < 0)
+    if (Minusseconds < 0)  //We add 60 in this case when the numbers being subtracted is less than 0. An example is when the number is -5, we add 60 to get 55 seconds.
     {
         int value3 = Minusseconds + 60;
         Minusseconds = value3;
